b632eadc40c27bdb75c6b95dc994dd8eafe5088b
George Chakhidze <0xfeeddeadbeef@gmail.com>
2025-03-07 23:02:59 +0400

    Bump major version to 3 due to breaking changes

--- src/TBC.Common.Configuration.Registry/Versioning.cs
+++ src/TBC.Common.Configuration.Registry/Versioning.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (C) TBC Bank. All Rights Reserved.
 
 /*
- * Pin assembly version to 2.0.0.0 because it is a part of the strong name;
+ * Pin assembly version to 3.0.0.0 because it is a part of the strong name;
  * to avoid assembly load exceptions and the need for manual binding redirects,
  * just pin it to a static version, even if the file version keeps on incrementing.
  * Only bump it when major version changes.
  */
-[assembly: System.Reflection.AssemblyVersion("2.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("3.0.0.0")]
